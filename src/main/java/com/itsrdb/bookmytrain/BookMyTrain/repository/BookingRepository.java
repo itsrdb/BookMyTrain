5bcc838db41d25761025b2f30e4219c32cd049f5
Rohit Borkar <rohitborkar00@gmail.com>
2024-04-25 03:19:53 +0530

    Fix schedule map table error

--- src/main/java/com/itsrdb/bookmytrain/BookMyTrain/repository/BookingRepository.java
+++ src/main/java/com/itsrdb/bookmytrain/BookMyTrain/repository/BookingRepository.java
@@ -1,6 +1,8 @@
 package com.itsrdb.bookmytrain.BookMyTrain.repository;
 
+import com.itsrdb.bookmytrain.BookMyTrain.model.Booking;
 import com.itsrdb.bookmytrain.BookMyTrain.model.Train;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,7 +10,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.time.LocalDate;
 
 @RepositoryRestResource
-public interface BookingRepository {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
     @Query("SELECT COUNT(b) FROM Booking b WHERE b.bookingDate = :date AND b.train = :train")
     long countBookingsByDateAndTrain(@Param("train") Train train, @Param("date") LocalDate date);
 }
